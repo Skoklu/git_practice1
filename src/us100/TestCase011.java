19fa9a22724485ae4682e184a81e67ed65ab83d4
selma <sunal.32@hotmail.com>
2022-04-09 22:30:25 +0100

    added hello functionality

--- src/us100/TestCase011.java
+++ src/us100/TestCase011.java
@@ -1,9 +1,10 @@
 package us100;
 
-public class TestCase001 {
+public class TestCase011 {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello EU8");
+        System.out.println("Hello All");
     }
+
+
 }
